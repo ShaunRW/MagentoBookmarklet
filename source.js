2531f133f6f318220554174936b98874d5af298f
ShaunRW <shaunw91@hotmail.com>
2015-01-22 22:10:56 +1100

    add the bookmarklet source code

---
+++ source.js
@@ -0,0 +1,19 @@
+// Check if the current page is a product, category, or something else.
+if(jQuery("body").hasClass("catalog-product-view")){ // is product page
+
+	// Get the product id from a field on the page that contains it.
+	id = jQuery(".no-display input").val()
+
+	// Openthe magento edit product page in a new tab.
+	window.open("http://www.gooutdoorsaustralia.com.au/shop/index.php/admin/catalog_product/edit/id/"+id,"_blank");
+
+}else if(jQuery("body").hasClass("catalog-category-view")){ // is category
+
+	// alert the user that the current page isnt supported
+	alert("Edit Category is not supported yet!");
+	
+}else{ // is something else
+
+	// alert the user that the current page isnt supported
+	alert("This page is not supported yet!");
+}
